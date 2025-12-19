f37a14c4ad82a742bf6853025c34ba1b88791295
hawarijawed <hawarijawed143@gmail.com>
2025-12-19 20:12:22 +0545

    First setup

---
+++ src/main/java/com/expenseShare/demo/ExpenseShareApplication.java
@@ -0,0 +1,13 @@
+package com.expenseShare.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expenseShare/demo/ExpenseShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenseShare.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
