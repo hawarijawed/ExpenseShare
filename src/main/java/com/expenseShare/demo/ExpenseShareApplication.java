4aa7f5c4dc3f30ce5c68f87168d71ce26c4cfd26
hawarijawed <hawarijawed143@gmail.com>
2025-12-19 20:46:13 +0545

    Setting up envs

--- src/main/java/com/expenseShare/demo/ExpenseShareApplication.java
+++ src/main/java/com/expenseShare/demo/ExpenseShareApplication.java
@@ -1,12 +1,21 @@
 package com.expenseShare.demo;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExpenseShareApplication {
 
+	@Value("${spring.name}")
+	private static String name;
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("name", dotenv.get("name"));
+
+		System.out.println("Environment variable: "+name);
 		SpringApplication.run(ExpenseShareApplication.class, args);
 	}
 
